93043b4529425d1647853aae8ee71f830452c006
Jin Liaskos <jinliaskos@gmail.com>
2017-05-05 10:08:18 +1000

    Paid and unpaid orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,12 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
 
+  def self.paid
+    Order.where.not(stripe_token: nil)
+  end
 
+  def self.unpaid
+    Order.where(stripe_token: nil)
+  end
 
 end
